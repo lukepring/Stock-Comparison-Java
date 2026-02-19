c85abddfcdcd1d3c77d1c24614881b5f960cd605
Luke Pring <hello@lukepr.ing>
2026-02-19 10:39:42 +0000

    Added test to ensure running and working

---
+++ src/main/java/uk/ac/roehampton/stockscope/HelloController.java
@@ -0,0 +1,12 @@
+package uk.ac.roehampton.stockscope;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+}
